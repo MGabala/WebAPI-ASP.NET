b58afe225d7f08425caa554d69cb9600c1080e76
gugimistrz666 <82496006+MGabala@users.noreply.github.com>
2022-04-06 11:02:07 +0200

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController, Route("api/products")]
+    public class ProductController : ControllerBase
+    {
+        //Pobierz całą listę 
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductDTO>> GetProducts()
+        {
+            return Ok(ProductsStore.CurrentProduct.Products);
+
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductDTO>> GetProductsCount()
+        {
+            return Ok(ProductsStore.CurrentProduct.Products.Count);
+        }
+        //Pobierz produkt o konkretnym ID
+        [HttpGet("{id}")]
+
+        public ActionResult<ProductDTO> GetProduct(int id)
+        {
+            var productToreturn = ProductsStore.CurrentProduct.Products.FirstOrDefault(x => x.Id == id);
+            if (productToreturn == null)
+            {
+                return NotFound();
+            }
+            return Ok(productToreturn);
+        }
+        //Dodaj nowy produkt i go zwróć
+        [HttpPost("{id}")]
+        public ActionResult<ProductDTO> AddProduct()
+        {
+            var products = ProductsStore.CurrentProduct.Products;
+            products.Add(new ProductDTO());
+            return Ok();
+            //  return NoContent();
+        }
+        //Aktualizacja Description i Quantity
+        [HttpPut]
+        public ActionResult<ProductDTO> UpdateProduct()
+        {
+            return NoContent();
+        }
+        //Usuwa produkt
+        [HttpDelete("{id}")]
+        public ActionResult<ProductDTO> DeleteProduct(int id)
+        {
+            var product = ProductsStore.CurrentProduct.Products;
+            product.RemoveAt(id);
+            return Ok(product);
+        }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string? Desc { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class ProductsStore
+    {
+        public List<ProductDTO> Products { get; set; }
+        public static ProductsStore CurrentProduct { get; } = new ProductsStore();
+        public ProductsStore()
+        {
+            Products = new List<ProductDTO>()
+            {
+                new ProductDTO()
+                {
+                    Id = 1,
+                    Name = "First Product",
+                    Desc = "This is description",
+                    Quantity = 5
+                },
+                new ProductDTO()
+                {
+                    Id=2,
+                    Name = "Second Product",
+                    Desc = String.Empty,
+                    Quantity  = 10
+                },
+                 new ProductDTO()
+                {
+                    Id=3,
+                    Name = "Third Product",
+                    Desc = "",
+                    Quantity  = 12
+                }
+            };
+        }
+    }
+
+}
