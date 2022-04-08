ec1215256abd44dea6bfd5e756ed597843a81d97
gugimistrz666 <82496006+MGabala@users.noreply.github.com>
2022-04-08 13:53:05 +0200

    update

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
