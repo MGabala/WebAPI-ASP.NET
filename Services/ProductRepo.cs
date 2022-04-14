838d4f0fcaead76da5d737313e55e0d1de000ebb
gugimistrz666 <82496006+MGabala@users.noreply.github.com>
2022-04-14 12:18:10 +0200

    update

---
+++ Services/ProductRepo.cs
@@ -0,0 +1,25 @@
+﻿namespace WebAPI_ASP.NET6.Services
+{
+    public class ProductRepo : IProductRepo
+    {
+        public Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product?> GetProductAsync(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TypeOfProduct?> GetTypeOfProduct(int productId, int typeofproductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TypeOfProduct>> GetTypeOfProductsAsync(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
