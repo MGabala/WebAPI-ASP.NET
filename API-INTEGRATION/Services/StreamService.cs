15695163937ed57d202a19e5b4733731b4215bfd
gugimistrz666 <82496006+MGabala@users.noreply.github.com>
2022-05-05 13:37:43 +0200

    Compression Mode supported.

--- API-INTEGRATION/Services/StreamService.cs
+++ API-INTEGRATION/Services/StreamService.cs
@@ -2,7 +2,11 @@
 {
     public class StreamService : IIntegrationService
     {
-        private static HttpClient _httpClient = new HttpClient();
+        //private static HttpClient _httpClient = new HttpClient();
+        private static HttpClient _httpClient = new HttpClient(new HttpClientHandler()
+        {
+            AutomaticDecompression = System.Net.DecompressionMethods.GZip
+        });
 
         public StreamService()
         {
@@ -15,7 +19,8 @@
         public async Task Run()
         {
             //await GetProductWithStream();
-            await GetProductWithStreamAndCompletionMode();
+            //await GetProductWithStreamAndCompletionMode();
+            await GetProductWithStreamAndCompletionModeWithCompression();
         }
 
         public async Task GetProductWithStream()
@@ -38,6 +43,19 @@
         public async Task GetProductWithStreamAndCompletionMode()
         {
             var request = new HttpRequestMessage(HttpMethod.Get, $"/api/products/3");
+            var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            using (var stream = await response.Content.ReadAsStreamAsync())
+            {
+                response.EnsureSuccessStatusCode();
+                var poster = stream.ReadAndDeserializeFromJson<IntegrationProduct>();
+            }
+        }
+        public async Task GetProductWithStreamAndCompletionModeWithCompression()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/api/products/3");
+
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
             var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
             using (var stream = await response.Content.ReadAsStreamAsync())
             {
